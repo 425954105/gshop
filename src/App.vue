965b17d01e5d61113f58fca76237ec687331617b
425954105 <37371161+425954105@users.noreply.github.com>
2019-05-27 21:38:38 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet.stylus">
+  .app
+    color: #666
+</style>
